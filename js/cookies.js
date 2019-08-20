91d7ebc4e3564da2db9d4b87463f11ddba0d301f
joanna-by <j.bykowska@gmail.com>
2019-08-20 20:26:17 +0200

    Created a cookies script

---
+++ js/cookies.js
@@ -0,0 +1,18 @@
+Cookies.set('MoozoMontownia', 'true', {expires: 0});
+
+const myCookie = Cookies.get('MoozoMontownia');
+
+
+function WHCheckCookies(){
+    if(!localStorage.cookies_accepted) {
+      var cookies_message = document.getElementById("cookies-message"); 
+      cookies_message.style.display="block" 
+    } 
+  } 
+  window.onload = WHCheckCookies;
+
+
+  function WHCloseCookiesWindow(){ 
+    localStorage.cookies_accepted = true; 
+    document.getElementById("cookies-message-container").removeChild(document.getElementById("cookies-message"));
+  }
